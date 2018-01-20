5f1dbc0b044b21e5ed3a2f35aa54be4e98135dd8
doganbirol13 <doganbirol13@gmail.com>
2018-01-20 16:48:37 +0300

    FirstCommit

---
+++ client/main.js
@@ -0,0 +1,33 @@
+
+import '/imports/startup/client';
+
+var Clappr = require("clappr");
+var Video360 = require("clappr-video360");
+
+Template.login.events({
+  'submit form': function(event){
+      event.preventDefault();
+      var emailVar = event.target.loginEmail.value;
+      var passwordVar = event.target.loginPassword.value;
+      Meteor.loginWithPassword(emailVar, passwordVar);
+  }
+});
+
+Template.officevideo.onRendered(function () {
+  // The URL to the 360 video player 
+  var Video360Url = '/SampleOfficeVideo1.mp4';
+
+  // Configure your Clappr player.
+  var PlayerInstance = new Clappr.Player({
+      source: Video360Url,
+      plugins: {
+          container: [Video360],
+      },
+      parentId: '#player',
+      width: 965
+  });
+
+  // Important to disable the click to pause native plugin of clappr
+  // otherwise you won't be able to use correctly the player
+  PlayerInstance.getPlugin('click_to_pause').disable();
+});
---
+++ imports/startup/client/routes.js
@@ -0,0 +1,72 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import { EJSON } from 'meteor/ejson';
+import { Router, RouteController } from 'meteor/iron:router';
+import '/client/main.html';
+
+
+Router.configure({
+    layoutTemplate: 'PageLayout'
+  });
+  
+  
+  Router.onBeforeAction(function () {
+    // all properties available in the route function
+    // are also available here such as this.params
+  
+    if (Meteor.userId()=="RAXNn7yekgGDddmzp") {
+      // if the user is not logged in, render the Login template
+      this.next();
+      this.redirect('/logged');
+    }
+    if (Meteor.userId()=="BkkiNSwjsys9MdNcR") {
+        // if the user is not logged in, render the Login template
+        this.next();
+        this.redirect('/logged2');   
+    } 
+    else {
+      // otherwise don't hold up the rest of hooks or our route/action function
+      // from running
+      this.next();
+      this.redirect('/');
+    }
+  });
+  
+  Router.route('/', function () {
+    this.render('welcome', {to: 'welcome'});
+    this.render('welcomepic', {to: 'welcomepic'});
+    this.render('nav2', {to: 'nav1'});
+    this.render('login', {to: 'login'});
+    this.render('void', {to: 'panoviewer'});
+  });
+  
+  Router.route('/logged', function () {
+    this.render('welcometext', {to: 'welcometext'});
+    this.render('void', {to: 'login'});
+    this.render('welcomepicmall', {to: 'welcomepic'});
+    this.render('nav2', {to: 'nav1'});
+    this.render('viewer', {to: 'panoviewer'});
+    this.render('anothertemplate', {to: 'anothertemplate'});
+  });
+  
+  Router.route('/logged2', function () {
+    this.render('void', {to: 'welcome'});
+    this.render('welcomepicoffice', {to: 'welcomepic'});
+    this.render('nav2', {to: 'nav1'});
+    this.render('void', {to: 'login'});
+    this.render('welcometext2', {to: 'welcometext'});
+    this.render('nav2', {to: 'nav1'});
+    this.render('officenavigation', {to: 'panoviewer'});
+    this.render('anothertemplate', {to: 'anothertemplate'});
+  });
+  
+  // Router.route('/', function () {
+  //   this.layout('PageLayout');
+  //   this.render('welcome', {to: 'welcome'});
+  //   this.render('welcomepic', {to: 'welcomepic'});
+  //   this.render('nav1', {to: 'nav1'});
+  //   this.render('nav2', {to: 'nav2'});
+  //   this.render('login', {to: 'login'});
+  // });
+  
